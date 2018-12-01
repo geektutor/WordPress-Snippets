37243843d12e7726a04be761e77297d9c36b20df
Geektutor <sodiq.akinjobi@gmail.com>
2018-12-01 02:37:14 +0100

    Renamed to cart-beside

--- cart-beside.php
+++ cart-beside.php
@@ -1,4 +1,4 @@
-/* This image allows me to add a picture to the cart page that is beside the pricing calculator. Useful for when you want to display a message there. Check image cart-beside.ng to see an example.
+/* This snippet allows me to add a picture to the cart page that is beside the pricing calculator. Useful for when you want to display a message there. Check image cart-beside.png to see an example.
 
 Usage: Simply chamge what is in the echo to what you want to show
 */
