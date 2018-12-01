b8c73c47cd95e79480f62885662aca1ff36a51b8
Geektutor <sodiq.akinjobi@gmail.com>
2018-12-01 02:28:23 +0100

    Added the Cart-beside snippet

---
+++ one.php
@@ -0,0 +1,11 @@
+/* This image allows me to add a picture to the cart page that is beside the pricing calculator. Useful for when you want to display a message there. Check image cart-beside.ng to see an example.
+
+Usage: Simply chamge what is in the echo to what you want to show
+*/
+
+add_action( 'woocommerce_cart_collaterals', 'processG', 10 );
+function processG() {
+echo '<img src="#" width="50%"/>';
+}
+
+
