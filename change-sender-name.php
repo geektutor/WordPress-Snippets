4c507913a3f9c5de4a152d73cebac7938bad6d26
Sodiq Akinjobi <sodiq.akinjobi@gmail.com>
2020-08-05 19:08:45 +0100

    Create change-sender-name.php

---
+++ change-sender-name.php
@@ -0,0 +1,11 @@
+ // Function to change email address
+ 
+function wpb_sender_email( $original_email_address ) {
+    return 'info@bybostore.com';
+}
+ 
+// Function to change sender name
+function wpb_sender_name( $original_email_from ) {
+    return 'ByboStore';
+}
+ 
